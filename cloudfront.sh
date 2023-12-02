00bc022d89dbf3336f866d04d0046efc5bef2906
Hemanthkumar441 <152642810+Hemanthkumar441@users.noreply.github.com>
2023-12-02 14:48:58 +0530

    Update cloudfront.sh

--- cloudfront.sh
+++ cloudfront.sh
@@ -1,3 +1,4 @@
+$stackname="Test-Demo-App"
 aws cloudformation create-stack \
-  --stack-name Test-Demo \
-  --template-body file://cfn.yaml --capabilities CAPABILITY_IAM --parameters ParameterKey=StackName,ParameterValue=Test-Demo ParameterKey=VpcId,ParameterValue=vpc-07a8c444b56dab6e9 ParameterKey=Environ,ParameterValue=dev 
+  --stack-name $stackname \
+  --template-body file://elastic-beanstalk.yaml.yaml  --parameters ParameterKey=StackName,ParameterValue=$stackname ParameterKey=VpcId,ParameterValue=vpc-07a8c444b56dab6e9 ParameterKey=Environ,ParameterValue=dev ParameterKey=Tags,ParameterValue=ParameterValue='[{\"Key\":\"NAME\",\"Value\":\"Demo-Test\"},{\"Key\":\"Env\",\"Value\":\"dev\"}]' --capabilities CAPABILITY_IAM
